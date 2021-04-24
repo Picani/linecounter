eb8f6d58adb5912881d6bc2e769c59f7e77a6cde
picani <picani@laposte.net>
2021-04-24 13:51:24 +0200

    Fix bad compressed files handling
    When a compressed file is passed, its content was not checked to be
    plain text. This is now fixed.

--- src/file_opener.rs
+++ src/file_opener.rs
@@ -21,17 +21,60 @@ pub fn open(path: &PathBuf) -> std::io::Result<Box<dyn BufRead>> {
 
         if tree_magic::match_filepath("text/plain", &path) {
             Ok(Box::new(BufReader::new(f)))
-        } else if tree_magic::match_filepath("application/gzip", &path) {
-            let d = GzDecoder::new(f);
-            Ok(Box::new( BufReader::new(d)))
-        } else if tree_magic::match_filepath("application/x-bzip2", &path) {
-            let d = BzDecoder::new(f);
-            Ok(Box::new(BufReader::new(d)))
         } else {
-            let mime_type = tree_magic::from_filepath(&path);
+            // Note that a buffer of 512 bytes will fail to correctly detect
+            // plain text of less than 512 bytes. I don't expect a less than
+            // 512 bytes text file to be compressed...
+            let mut magic_buffer: [u8; 512] = [0; 512];
+            let mut mime_type = tree_magic::from_filepath(&path);
+            let mut compressed = false;
+
+            // TODO: Clean all that...
+
+            if tree_magic::match_filepath("application/gzip", &path) {
+                compressed = true;
+
+                let mut d = GzDecoder::new(f);
+                d.read(&mut magic_buffer)?;
+
+                if tree_magic::match_u8("text/plain", &magic_buffer) {
+                    // We re-open the file and the decompress stream because
+                    // we want to go back to the file start. If we use
+                    // d.get_mut().seek(), we corrupt the stream.
+                    let f = File::open(&path)?;
+                    let d = GzDecoder::new(f);
+                    return Ok(Box::new(BufReader::new(d)));
+                } else {
+                    mime_type = tree_magic::from_u8(&magic_buffer);
+                }
+            } else if tree_magic::match_filepath("application/x-bzip2", &path) {
+                compressed = true;
+
+                let mut d = BzDecoder::new(f);
+                d.read(&mut magic_buffer)?;
+
+                if tree_magic::match_u8("text/plain", &magic_buffer) {
+                    // Idem here
+                    let f = File::open(&path)?;
+                    let d = BzDecoder::new(f);
+                    return Ok(Box::new(BufReader::new(d)));
+                } else {
+                    mime_type = tree_magic::from_u8(&magic_buffer);
+                }
+            }
+
+            // If we're here, then neither the file nor its decompressed
+            // content is supported.
+
+            let err_msg = if compressed {
+                format!("Unsupported compressed content: {}", mime_type)
+            } else {
+                format!("Unsupported file type: {}", mime_type)
+            };
+
             Err(Error::new(
                 ErrorKind::InvalidData,
-                format!("Unsupported file type: {}", mime_type),
+                err_msg,
             ))
         }
     }
