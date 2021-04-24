f650b25c7c36adae27aeade8c6f9f788f7ad7a98
picani <picani@laposte.net>
2021-04-24 14:01:38 +0200

    Use tree_magic_mini instead of tree_magic

--- src/file_opener.rs
+++ src/file_opener.rs
@@ -19,25 +19,26 @@ pub fn open(path: &PathBuf) -> std::io::Result<Box<dyn BufRead>> {
     } else {
         let f = File::open(&path)?;
 
-        if tree_magic::match_filepath("text/plain", &path) {
+        if tree_magic_mini::match_filepath("text/plain", &path) {
             Ok(Box::new(BufReader::new(f)))
         } else {
             // Note that a buffer of 512 bytes will fail to correctly detect
             // plain text of less than 512 bytes. I don't expect a less than
             // 512 bytes text file to be compressed...
             let mut magic_buffer: [u8; 512] = [0; 512];
-            let mut mime_type = tree_magic::from_filepath(&path);
+            // Safe unwrap because the file is already tested.
+            let mut mime_type  = tree_magic_mini::from_filepath(&path).unwrap();
             let mut compressed = false;
 
             // TODO: Clean all that...
 
-            if tree_magic::match_filepath("application/gzip", &path) {
+            if tree_magic_mini::match_filepath("application/gzip", &path) {
                 compressed = true;
 
                 let mut d = GzDecoder::new(f);
                 d.read(&mut magic_buffer)?;
 
-                if tree_magic::match_u8("text/plain", &magic_buffer) {
+                if tree_magic_mini::match_u8("text/plain", &magic_buffer) {
                     // We re-open the file and the decompress stream because
                     // we want to go back to the file start. If we use
                     // d.get_mut().seek(), we corrupt the stream.
@@ -45,21 +46,21 @@ pub fn open(path: &PathBuf) -> std::io::Result<Box<dyn BufRead>> {
                     let d = GzDecoder::new(f);
                     return Ok(Box::new(BufReader::new(d)));
                 } else {
-                    mime_type = tree_magic::from_u8(&magic_buffer);
+                    mime_type = tree_magic_mini::from_u8(&magic_buffer);
                 }
-            } else if tree_magic::match_filepath("application/x-bzip2", &path) {
+            } else if tree_magic_mini::match_filepath("application/x-bzip2", &path) {
                 compressed = true;
 
                 let mut d = BzDecoder::new(f);
                 d.read(&mut magic_buffer)?;
 
-                if tree_magic::match_u8("text/plain", &magic_buffer) {
+                if tree_magic_mini::match_u8("text/plain", &magic_buffer) {
                     // Idem here
                     let f = File::open(&path)?;
                     let d = BzDecoder::new(f);
                     return Ok(Box::new(BufReader::new(d)));
                 } else {
-                    mime_type = tree_magic::from_u8(&magic_buffer);
+                    mime_type = tree_magic_mini::from_u8(&magic_buffer);
                 }
             }
 
